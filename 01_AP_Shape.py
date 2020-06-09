ba9e22fd1eca7f92f7dd81cdc8ca09e7ef022353
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-06-10 10:52:36 +1200

    fullly functioning asking user for shape

--- 01_AP_Shape.py
+++ 01_AP_Shape.py
@@ -14,6 +14,7 @@ def shape_checker(question, error):
 
         # continues if valid shape is chosen
         if shape in possible_shapes:
+            print("Good job!")
             break
         # prints error message
         else:
@@ -25,8 +26,9 @@ possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram",
 
 # Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
 # .join removes brackets and ' when printing out the list and adds a space between each item
-choices =(' ' .join(possible_shapes))
+choices =(', ' .join(possible_shapes))
 
 # asks user for shape
-shape = shape_checker("Please choose a shape to pick from: {} ".format(choices), "Oops please choose a shape from the list!")
+for item in possible_shapes:
+    shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
 
--- 02_AP_Formula.py
+++ 02_AP_Formula.py
@@ -2,3 +2,11 @@
 
 # To do:
 # Get formulas for the area and perimeter for each shape
+
+# used for pi
+import math
+print(math.pi)
+
+# Circle
+area_circle = math.pi * radius^2
+print(area_circle)
