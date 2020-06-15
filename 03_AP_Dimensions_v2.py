5a287ca690927ab222e773a814aeccfc459d2f96
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-06-15 18:26:00 +1200

    Revert "Merge branch 'master' of https://github.com/massey-high-school/2020-91896-7-assessment-sampangk71280"
    This reverts commit d4f04e0eccab7f2e77921fc7a3cb3bd637f6a9df, reversing
    changes made to 535b7f8c3dc659d5e4e64f55f58adfcb9c0775a4.

--- 03_AP_Dimensions_v2.py
+++ 03_AP_Dimensions_v2.py
@@ -23,12 +23,12 @@ def shape_checker(question, error):
 
 # Number checking function
 # recycled from previous programs and edited
-def intcheck(question):
+def intcheck(question, number):
     valid = False
     while not valid:
-        number = 0
         # Error message
         error = "Whoops! Please enter a valid number above {}!".format(number)
+
         try:
             response = float(input(question))
 
@@ -44,72 +44,59 @@ def intcheck(question):
             print(error)
 
 
-def dimensions_needed(shape):
-    if shape == "circle":
-        # gets radius
-        radius = intcheck("Radius: ")
-        # calculates area and perimeter
-        area = math.pi * radius ** 2
-        perimeter = 2 * math.pi * radius
+def dimensions_needed(shape, length_type):
+    length_type = ["length", "width", "base", "height", "side_a", "side_b", "side_c", "side_d"]
 
-    elif shape == "square" or shape == "rectangle":
-        # gets length
-        length = intcheck("Length: ")
-        # calculates area and perimeter of a square
-        area = length * length
-        perimeter = length * 4
-        if shape == "rectangle":
-            # gets width
-            width = intcheck("Width: ")
-            # calculates area and perimeter of a rectangle
-            area = length * width
-            perimeter = (2 * length) + (2 * width)
+    length = intcheck("Length: ", 0)
+    width = intcheck("Width: ", 0)
+    base = intcheck("Base: ", 0)
+    height = intcheck("Height: ", 0)
+    side_a = intcheck("Side A: ", 0)
+    side_b = intcheck("Side B: ", 0)
+    side_c = intcheck("Side C: ", 0)
+    side_d = intcheck("Side D: ", 0)
 
-    elif shape == "triangle" or shape == "parallelogram" or shape == "trapezium":
-        # gets height
-        height = intcheck("Height: ")
-        side_a = intcheck("Side A: ")
-        side_b = intcheck("Side B: ")
-        if shape == "parallelogram" or shape == "triangle":
-            base = intcheck("Base: ")
-        elif shape == "triangle" or "trapezium":
-            side_c = intcheck("Side C: ")
-        elif shape == "trapezium":
-            side_d = intcheck("Side D: ", 0)
+# set lengths to zero until needed
+radius = 0
+length = 0
+width = 0
+base = 0
+height = 0
+side_a = 0
+side_b = 0
+side_c = 0
+side_d = 0
 
-    # formats statements 
-    print("-----{}-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(shape,area,perimeter))
-    return area, perimeter
-
-
-
-keep_going = ""
-while keep_going == "":
-
-    # set lengths to zero until needed
-    radius = 0
-    length = 0
-    width = 0
-    base = 0
-    height = 0
-    side_a = 0
-    side_b = 0
-    side_c = 0
-    side_d = 0
+# Formula list
+# circles
+circle_area = math.pi * radius**2
+circle_perimeter = 2 * math.pi * radius
+# squares
+square_area = length * 2
+square_perimeter = length * 4
+# rectangles
+rectangle_area = length * width
+rectangle_perimeter = (2 * length) + (2 * width)
+# triangle
+triangle_area = 0.5 * base * height
+triangle_perimeter = side_a + side_b +side_c
+# parallelogram
+parallelogram_area = base * height
+parallolgram_perimeter = 2 * (side_a + side_b)
+# trapezium
+trapezium_area = (side_a + side_b)/2 * height
+trapezium_perimeter = side_a + side_b + side_c + side_d
 
 
+# Shapes available
+possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram", "trapezium"]
 
-    # Shapes available
-    possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram", "trapezium"]
+# Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
+# .join removes brackets and ' when printing out the list and adds a space between each item
+choices =(', ' .join(possible_shapes))
 
-    # Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
-    # .join removes brackets and ' when printing out the list and adds a space between each item
-    choices =(', ' .join(possible_shapes))
-
-    # asks user for shape
-    shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
-    dimensions = dimensions_needed(shape)
-    # print(dimensions)
+# asks user for shape
+shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
 
 
 
