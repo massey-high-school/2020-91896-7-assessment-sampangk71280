b45f5e901bb9e23acf7604ecf4297deff4e825ba
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-06-16 12:03:59 +1200

    working calculate_area function

--- 03_AP_Dimensions.py
+++ 03_AP_Dimensions.py
@@ -43,85 +43,86 @@ def intcheck(question, number):
         except ValueError:
             print(error)
 
+keep_going = ""
+while keep_going == "":
+    # Shapes available
+    possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram", "trapezium"]
 
-# Shapes available
-possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram", "trapezium"]
+    # Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
+    # .join removes brackets and ' when printing out the list and adds a space between each item
+    choices =(', ' .join(possible_shapes))
 
-# Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
-# .join removes brackets and ' when printing out the list and adds a space between each item
-choices =(', ' .join(possible_shapes))
+    # asks user for shape
+    shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
 
-# asks user for shape
-shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
+    # user chooses circle
+    if shape == "circle":
+        # gets radius
+        radius = intcheck("Radius: ", 0)
+        # calculates area and perimeter
+        circle_area = math.pi * radius ** 2
+        circle_perimeter = 2 * math.pi * radius
+        print("-----Circle----- \nArea: {:.2f}\nPerimeter: {:.2f}\n".format(circle_area, circle_perimeter))
 
-# user chooses circle
-if shape == "circle":
-    # gets radius
-    radius = intcheck("Radius: ", 0)
-    # calculates area and perimeter
-    circle_area = math.pi * radius ** 2
-    circle_perimeter = 2 * math.pi * radius
-    print("-----Circle----- \nArea: {:.2f}\nPerimeter: {:.2f}\n".format(circle_area, circle_perimeter))
+    # user chooses square
+    elif shape == "square":
+        # gets length
+        length = intcheck("Length: ", 0)
+        # calculates area and perimeter
+        square_area = length * length
+        square_perimeter = length * 4
+        print("-----Square-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(square_area, square_perimeter))
 
-# user chooses square
-elif shape == "square":
-    # gets length
-    length = intcheck("Length: ", 0)
-    # calculates area and perimeter
-    square_area = length * length
-    square_perimeter = length * 4
-    print("-----Square-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(square_area, square_perimeter))
+    # user chooses rectangle
+    elif shape == "rectangle":
+        # gets length and width
+        length = intcheck("Length: ", 0)
+        width = intcheck("Width: ", 0)
+        # calculates area and perimeter
+        rectangle_area = length * width
+        rectangle_perimeter = (2 * length) + (2 * width)
+        print("-----Rectangle-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(rectangle_area, rectangle_perimeter))
 
-# user chooses rectangle
-elif shape == "rectangle":
-    # gets length and width
-    length = intcheck("Length: ", 0)
-    width = intcheck("Width: ", 0)
-    # calculates area and perimeter
-    rectangle_area = length * width
-    rectangle_perimeter = (2 * length) + (2 * width)
-    print("-----Rectangle-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(rectangle_area, rectangle_perimeter))
+    # user chooses triangle
+    elif shape == "triangle":
+        # gets base, height, and sides
+        base = intcheck("Base: ", 0)
+        height = intcheck("Height: ", 0)
+        side_a = intcheck("Side A: ", 0)
+        side_b = intcheck("Side B: ", 0)
+        side_c = intcheck("Side C: ", 0)
+        # calculates area and perimeter
+        triangle_area = 0.5 * base * height
+        triangle_perimeter = side_a + side_b + side_c
+        print("-----Triangle-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(triangle_area, triangle_perimeter))
 
-# user chooses triangle
-elif shape == "triangle":
-    # gets base, height, and sides
-    base = intcheck("Base: ", 0)
-    height = intcheck("Height: ", 0)
-    side_a = intcheck("Side A: ", 0)
-    side_b = intcheck("Side B: ", 0)
-    side_c = intcheck("Side C: ", 0)
-    # calculates area and perimeter
-    triangle_area = 0.5 * base * height
-    triangle_perimeter = side_a + side_b + side_c
-    print("-----Triangle-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(triangle_area, triangle_perimeter))
+    # user chooses parallelogram
+    elif shape == "parallelogram":
+        # gets base, height, and sides
+        base = intcheck("Base: ", 0)
+        height = intcheck("Height: ", 0)
+        side_a = intcheck("Side A: ", 0)
+        side_b = intcheck("Side B: ", 0)
+        # calculates area and perimeter
+        parallelogram_area = base * height
+        parallolgram_perimeter = 2 * (side_a + side_b)
+        print("-----Parallelogram-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(parallelogram_area, parallolgram_perimeter))
 
-# user chooses parallelogram
-elif shape == "parallelogram":
-    # gets base, height, and sides
-    base = intcheck("Base: ", 0)
-    height = intcheck("Height: ", 0)
-    side_a = intcheck("Side A: ", 0)
-    side_b = intcheck("Side B: ", 0)
-    # calculates area and perimeter
-    parallelogram_area = base * height
-    parallolgram_perimeter = 2 * (side_a + side_b)
-    print("-----Parallelogram-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(parallelogram_area, parallolgram_perimeter))
+    # user chooses trapezium
+    elif shape == "trapezium":
+        # gets height and sides
+        height = intcheck("Height: ", 0)
+        side_a = intcheck("Side A: ", 0)
+        side_b = intcheck("Side B: ", 0)
+        side_c = intcheck("Side C: ", 0)
+        side_d = intcheck("Side D: ", 0)
+        # calculates area and perimeter
+        trapezium_area = (side_a + side_b) / 2 * height
+        trapezium_perimeter = side_a + side_b + side_c + side_d
+        print("-----Trapezium-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(trapezium_area, trapezium_perimeter))
 
-# user chooses trapezium
-elif shape == "trapezium":
-    # gets height and sides
-    height = intcheck("Height: ", 0)
-    side_a = intcheck("Side A: ", 0)
-    side_b = intcheck("Side B: ", 0)
-    side_c = intcheck("Side C: ", 0)
-    side_d = intcheck("Side D: ", 0)
-    # calculates area and perimeter
-    trapezium_area = (side_a + side_b) / 2 * height
-    trapezium_perimeter = side_a + side_b + side_c + side_d
-    print("-----Trapezium-----\nArea: {:.2f}\nPerimeter: {:.2f}\n".format(trapezium_area, trapezium_perimeter))
-
-else:
-    print("errrorrr")
+    else:
+        print("errrorrr")
 
 
 
---
+++ 04_AP_Ask.py
@@ -0,0 +1,114 @@
+# Area Perimeter - Component
+
+# To do:
+# Create two functions to ask if they want to calculate area or perimeter
+
+
+# used for pi
+import math
+
+# functions
+# Checks that the user choices a shape from the list
+def shape_checker(question, error):
+    valid = False
+    while not valid:
+        # question
+        shape = input(question).lower()
+
+        # continues if valid shape is chosen
+        if shape in possible_shapes:
+            return shape
+        # prints error message
+        else:
+            print(error)
+
+# Number checking function
+# recycled from previous programs and edited
+def intcheck(question):
+    valid = False
+    while not valid:
+        number = 0
+        # Error message
+        error = "Whoops! Please enter a valid number above {}!".format(number)
+        try:
+            response = float(input(question))
+
+            # Returns if response is bigger than number
+            if number < response:
+                return response
+            # If response is less than number, print out error, and keep asking until valid input
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+# calculates the area
+def calculate_area(shape):
+    if shape == "circle":
+        radius = intcheck("Radius: ") # only circle needs radius
+        area = math.pi * radius ** 2 # area of a circle
+
+    elif shape == "square" or shape == "rectangle":
+        length = intcheck("Length: ") # both square and rectangle needs length for area
+        area = length * length # area of a square
+        if shape == "rectangle":
+            width = intcheck("Width: ") # only rectangle needs width for area
+            area = length * width # area of a rectangle
+
+    elif shape == "triangle" or shape == "parallelogram":
+        # both triangle and parallelogram need base and height for area
+        base = intcheck("Base: ")
+        height = intcheck("Height: ")
+        if shape == "triangle":
+            area = 0.5 * base * height # area of triangle
+        if shape == "parallelogram":
+            area = base * height # area of parallelogram
+
+    elif shape == "trapezium":
+        side_a = intcheck("Side A: ")
+        side_b = intcheck("Side B: ")
+        height = intcheck("Height: ")
+        area = (side_a + side_b) / 2 * height # area of trapezium
+
+    return area
+
+
+# Main Routine
+# Loops entire calculator
+keep_going = ""
+while keep_going == "":
+
+    # set lengths to zero until needed
+    radius = 0
+    length = 0
+    width = 0
+    base = 0
+    height = 0
+    side_a = 0
+    side_b = 0
+    side_c = 0
+    side_d = 0
+
+    # Shapes available
+    possible_shapes = ["circle", "square", "rectangle", "triangle", "parallelogram", "trapezium"]
+
+    # Code from https://www.edureka.co/community/1792/how-to-print-array-list-without-brackets-in-python
+    # .join removes brackets and ' when printing out the list and adds a space between each item
+    choices =(', ' .join(possible_shapes))
+
+    # asks user for shape
+    shape = shape_checker("Please choose a shape to pick from: {} \nShape: ".format(choices), "Oops please choose a shape from the list!")
+    area_or_perimeter = input("Do you want to calculate area, perimeter or both?").lower()
+    if area_or_perimeter == "area":
+        area = calculate_area(shape)
+        print("-----{}-----\nArea: {:.2f}\n".format(shape, area))
+    elif area_or_perimeter == "perimeter":
+    elif area_or_perimeter == "both":
+    else:
+        print("Please choose!")
+
+
+
+
